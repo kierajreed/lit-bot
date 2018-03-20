5e77304b295f4e76cdb376bdf1bf42024fb3026c
SkyHawkB <danielmey626@gmail.com>
2018-03-20 18:37:55 -0400

    make a udict

--- commands/udict.js
+++ commands/udict.js
@@ -1,5 +1,29 @@
+const uDict = require('urban-dictionary');
+const Discord = require('discord.js');
+
 module.exports.run = (client, message, args, config) => {
-  if(args.length < 1 || args.length > 2) return message.channel.send(config.messages.udict.noArgError);
+  if(args.length < 1) return message.channel.send(config.messages.udict.argError);
+  let word = '';
+  for(let i = 0; i < args.length; i++) {
+    word += args[i];
+    if(i < args.length - 1) {
+      word += ' ';
+    }
+  }
+  word = word.substring(0, word.length);
+  uDict.term(word, (error, entries, tags, sounds) => {
+    if(error) {
+      console.log("ERROR: " + error.message);
+      message.channel.send(error.message + " Sorry.");
+    } else {
+      const embed = new Discord.MessageEmbed()
+        .setColor(914964)
+        .setTitle(entries[0].word)
+        .addField('Definition:',  entries[0].definition)
+        .addField('Example:', entries[0].example);
+      message.channel.send(embed);
+    }
+});
 };
 
 module.exports.info = {
